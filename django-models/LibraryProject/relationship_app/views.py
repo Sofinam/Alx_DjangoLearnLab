b87f613ac7c1a4c3e0b6bf287770cac1eb48762e
Sofinam <omukowus@gmail.com>
2025-03-07 14:03:52 +0100

    changed views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .models import Book
 from django.views.generic.detail import DetailView
+from .models import Book
 from .models import Library
 
 # Function based view
