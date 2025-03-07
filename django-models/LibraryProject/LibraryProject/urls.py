eb85badb5081945e7733feae52f711cfef65348c
Sofinam <omukowus@gmail.com>
2025-03-07 21:51:10 +0100

    made edit to url.py

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from relationship_app.views import list_books
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('relationship/', include('relationship_app.urls')),
+    path('', list_books, name='home'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,10 +6,10 @@ from .models import Library
 # Function based view
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    return render(request, "relationship_app/list_books.html", {"books": books})
 #class based view
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
 
