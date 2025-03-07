034a7e3710e4fd6015d02031b4243d07b30d8670
Sofinam <omukowus@gmail.com>
2025-03-07 23:58:54 +0100

    made new edits to urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import list_books, LibraryDetailView, register_view, login_view, logout_view
+from .views import list_books, LibraryDetailView
+from .views import register_view
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
