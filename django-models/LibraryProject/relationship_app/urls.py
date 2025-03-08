da42b9ba1861efea9c17c022ec2bf7f62e146e96
Sofinam <omukowus@gmail.com>
2025-03-08 22:08:55 +0100

    made changes to 3 files

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -12,5 +12,4 @@ urlpatterns = [
     path('admin/', admin_view, name='admin_view'),
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
-
 ]
