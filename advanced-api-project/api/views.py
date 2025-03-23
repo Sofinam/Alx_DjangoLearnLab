a279a9e18dae6aba94f5ec1ceb451473b41914f4
Sofinam <omukowus@gmail.com>
2025-03-23 09:56:51 +0100

    made changes in views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -12,10 +12,11 @@ Example Queries:
 
 
 from django.shortcuts import render
-from rest_framework import generics, permissions, filters
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, permissions, filters
 from .models import Book
 from .serializers import BookSerializer
+from django_filters import rest_framework
 
 # Create your views here.
 # List all books (read-only)
